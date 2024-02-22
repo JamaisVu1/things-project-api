2a51be8d29a092aa6728444d0ae61d7902a1bf1d
zach <zach.moore0628@gmail.com>
2024-02-21 19:20:22 -0600

    lab 33

--- django_stuff_project/django_stuff_project/urls.py
+++ django_stuff_project/django_stuff_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/stuff/", include("stuff.urls")),
     path('api-auth/', include('rest_framework.urls')),  
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair' ), # new lab 33
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'), # new lab 33
 ]
